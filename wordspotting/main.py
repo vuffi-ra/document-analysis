3b44dfe66005cd37ce4051137fe8ee30464d0e6f
Markus Muegge <markus.muegge@t-online.de>
2021-01-26 16:33:00 +0100

    Code cleanup and prettier plots

--- wordspotting/main.py
+++ wordspotting/main.py
@@ -1,11 +1,10 @@
 import math
 import pickle
 import os
-import cv2
-import numpy as np
-import tqdm
 from collections import defaultdict
 
+import cv2
+import numpy as np
 import PIL.Image as Image
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
@@ -14,7 +13,30 @@ from scipy.spatial.distance import cdist
 
 from wordspotting.SIFT.compute_sift import compute_sift_descriptors
 
-show_plots = True
+
+class WordspottingSettings:
+    """Sift Step Size. Number of pixels between two SIFT descriptors."""
+    sift_step_size = 5
+    """Sift Cell Size. Width and height in pixels of a SIFT descriptor cell. Each descriptor is 4x4 cells large."""
+    sift_cell_size = 15
+    """Width of a heatmap cell."""
+    heatmap_grid_width = 100
+    """Height of a heatmap cell."""
+    heatmap_grid_height = 100
+    """Heatmap threshold. 
+    Only patches that contain at least one pixel with a value above the threshold are considered.
+    """
+    patch_threshold = 0.75
+    """Number of entries in the visual dictionary. Equivalent to number of centroids in Lloyd's Algorithm"""
+    dictionary_size = 100
+    """Number of iterations for LLoyd's Algorithm"""
+    lloyd_iterations = 20
+    """Non-Maximum-Supression Threshold. NMS will remove all patches that overlap more than this threshold"""
+    nms_threshold = 0.1
+    """Determines how the heatmap is interpolated to the image resolution"""
+    heatmap_interpolation_method = cv2.INTER_CUBIC
+    """Should plots be shown while calculating patches"""
+    show_plots = True
 
 
 class Patch:
@@ -83,69 +105,42 @@ def inverse_file_structure(frames, labels):
 
 
 def gen_heatmap(ifs, image_width, image_height, grid_width, grid_height, query_words):
-    map = np.zeros((math.ceil(image_height / grid_height), math.ceil(image_width / grid_width)))
+    hmap = np.zeros((math.ceil(image_height / grid_height), math.ceil(image_width / grid_width)))
     for word in query_words:
         for (x, y) in ifs[word]:
             x_grid = math.floor(x / grid_width)
             y_grid = math.floor(y / grid_height)
-            map[y_grid, x_grid] += 1
-    return map
+            hmap[y_grid, x_grid] += 1
 
-
-def show_patches(image, patches, heatmap):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.imshow(image, cmap="Greys_r")
-    ax.imshow(heatmap, cmap="Reds", alpha=0.4)
-
-    for patch in patches:
-        lower_left = (patch.top_left[0], patch.bottom_right[1])
-        width = (patch.bottom_right[0] - patch.top_left[0])
-        height = (patch.bottom_right[1] - patch.top_left[1])
-        rect = Rectangle(lower_left, width, -height, alpha=0.5)
-        ax.add_patch(rect)
-
-    plt.show()
+    hmap = hmap / np.max(hmap)
+    return hmap
 
 
 # query_word is a numpy matrix of the image data
-def wordspotting(page, query_word, step_size, cell_size, grid_width, grid_height, threshold):
-
+def wordspotting(page, query_word, settings: WordspottingSettings):
     document_image_filename = os.path.join(os.path.dirname(__file__), 'pages/%s.png' % page)
     image = Image.open(document_image_filename)
     # Fuer spaeter folgende Verarbeitungsschritte muss das Bild mit float32-Werten vorliegen.
     im_arr = np.asarray(image, dtype='float32')
 
-    pickle_densesift_fn = 'SIFT/%s-full_dense-%d_sift-%d_descriptors.p' % (page, step_size, cell_size)
+    pickle_densesift_fn = 'SIFT/%s-full_dense-%d_sift-%d_descriptors.p' \
+                          % (page, settings.sift_step_size, settings.sift_cell_size)
     frames, descriptors = pickle.load(open(pickle_densesift_fn, 'rb'))
 
-    n_centroids = 100
-    visual_words, labels = kmeans2(descriptors, n_centroids, iter=20, minit='points')
+    visual_words, labels = kmeans2(descriptors, settings.dictionary_size, iter=settings.lloyd_iterations,
+                                   minit='points')
 
     ifs = inverse_file_structure(frames, labels)
 
-    query_frames, query_desc = compute_sift_descriptors(query_word, cell_size, step_size)
+    query_frames, query_desc = compute_sift_descriptors(query_word, settings.sift_cell_size, settings.sift_step_size)
 
+    # noinspection PyUnresolvedReferences
     query_words = np.argmin(cdist(visual_words, query_desc), axis=0)
 
-    # Find image size
-    heatmap = gen_heatmap(ifs, im_arr.shape[1], im_arr.shape[0], grid_width, grid_height, query_words)
-
-    heatmap_normalized = heatmap / np.max(heatmap)
-    heatmap_threshold = np.where(heatmap_normalized >= threshold, heatmap_normalized, 0.0)
-
-    heatmap_threshold = cv2.resize(heatmap_threshold, dsize=(image.width, image.height), interpolation=cv2.INTER_CUBIC)
-    heatmap_interpolated = cv2.resize(heatmap_normalized, dsize=(image.width, image.height), interpolation=cv2.INTER_CUBIC)
-
-    if show_plots:
-        plt.imshow(heatmap_interpolated)
-        plt.show()
-        plt.imshow(image)
-        plt.imshow(heatmap_threshold, alpha=0.5)
-        plt.show()
-        plt.imshow(image)
-        plt.imshow(heatmap_interpolated, alpha=0.5)
-        plt.show()
+    heatmap = gen_heatmap(ifs, im_arr.shape[1], im_arr.shape[0], settings.heatmap_grid_width,
+                          settings.heatmap_grid_height, query_words)
+    heatmap = cv2.resize(heatmap, dsize=(image.width, image.height),
+                         interpolation=settings.heatmap_interpolation_method)
 
     patch_width = query_word.shape[1]
     patch_height = query_word.shape[0]
@@ -160,21 +155,43 @@ def wordspotting(page, query_word, step_size, cell_size, grid_width, grid_height
     for x in range(start_x, im_arr.shape[1], step_x):
         for y in range(start_y, im_arr.shape[0], step_y):
             patch = Patch(x, y, patch_width, patch_height, 0.0)
-            hmax = heatmap_interpolated[int(patch.top_left[1]):int(patch.bottom_right[1]),
-                          int(patch.top_left[0]):int(patch.bottom_right[0])].max()
-            patch.score = heatmap_interpolated[int(patch.top_left[1]):int(patch.bottom_right[1]),
-                          int(patch.top_left[0]):int(patch.bottom_right[0])].sum()
-            if hmax > threshold:
+            hmax = heatmap[int(patch.top_left[1]):int(patch.bottom_right[1]),
+                           int(patch.top_left[0]):int(patch.bottom_right[0])].max()
+            patch.score = heatmap[int(patch.top_left[1]):int(patch.bottom_right[1]),
+                                  int(patch.top_left[0]):int(patch.bottom_right[0])].sum()
+            if hmax > settings.patch_threshold:
                 patches.append(patch)
 
-    print("Patches found")
+    nms_patches = nms(patches, settings.nms_threshold)
 
-    nms_patches = nms(patches, 0.1)
+    if settings.show_plots:
+        fig = plt.figure()
+        heatmap_plot = fig.add_subplot(1, 3, 1)
+        threshold_plot = fig.add_subplot(1, 3, 2)
+        patch_plot = fig.add_subplot(1, 3, 3)
 
-    if show_plots:
-        show_patches(im_arr, nms_patches, heatmap_threshold)
+        heatmap_plot.set_title("Heatmap (Interpolated)")
+        heatmap_plot.imshow(image, cmap="Greys_r")
+        heatmap_plot.imshow(heatmap, cmap="Reds", alpha=0.4)
 
-    print("Done")
+        heatmap_threshold = np.where(heatmap >= settings.patch_threshold, heatmap, 0.0)
+        threshold_plot.set_title("Heatmap (Interpolated, Threshold: %g)" % settings.patch_threshold)
+        threshold_plot.imshow(image, cmap="Greys_r")
+        threshold_plot.imshow(heatmap_threshold, cmap="Reds", alpha=0.4)
+
+        patch_plot.set_title("NMS Patches")
+        patch_plot.imshow(image, cmap="Greys_r")
+        patch_plot.imshow(heatmap, cmap="Reds", alpha=0.4)
+
+        for patch in nms_patches:
+            lower_left = (patch.top_left[0], patch.bottom_right[1])
+            width = (patch.bottom_right[0] - patch.top_left[0])
+            height = (patch.bottom_right[1] - patch.top_left[1])
+            rect = Rectangle(lower_left, width, -height, alpha=0.5)
+            patch_plot.add_patch(rect)
+
+        plt.show()
+
     return nms_patches
 
 
@@ -194,7 +211,9 @@ def find_relevant_patches(patches, gt):
 def analyze(patches, gt):
     if not patches or not gt:
         return 0.0, 0.0, 0.0
+
     relevancy_list = find_relevant_patches(patches, gt)
+
     precisions = []
     for i in range(1, len(relevancy_list) + 1):
         precisions.append(sum(relevancy_list[0:i]) / i)
@@ -228,16 +247,9 @@ def read_groundtruth(page):
 
 def main():
     page = "2700270"
-    step_size = 5
-    cell_size = 15
 
-    grid_height = 100
-    grid_width = 100
-
-    threshold = 0.8
-
-    input = cv2.imread("inputs/the.png")
-    patches = wordspotting(page, input, step_size, cell_size, grid_width, grid_height, threshold)
+    input_word = cv2.imread("inputs/the.png")
+    patches = wordspotting(page, input_word, WordspottingSettings())
     gt = read_groundtruth(page)
 
     precision, recall, average_precision = analyze(patches, gt["the"])

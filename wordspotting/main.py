1e1b6bbb2b529c919ed0658bf216dfc0e55c5343
Markus Muegge <markus.muegge@t-online.de>
2021-02-02 16:19:25 +0100

    Full page analysis

--- wordspotting/main.py
+++ wordspotting/main.py
@@ -4,7 +4,7 @@ import math
 import pickle
 import os
 from collections import defaultdict
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Union
 
 import cv2
 import numpy as np
@@ -32,6 +32,10 @@ class WordspottingSettings:
     Only patches that contain at least one pixel with a value above the threshold are considered. [0, 1]
     """
     patch_threshold: float = 0.75
+    """Patch Step. Determines the step size used when walking the heatmap with possible patches.
+    Step in pixels when value above 1, else factor of query patch size.
+    """
+    patch_step: Tuple[Union[int, float], Union[int, float]] = 1 / 8, 1 / 8
     """Number of entries in the visual dictionary. Equivalent to number of centroids in Lloyd's Algorithm"""
     dictionary_size: int = 100
     """Number of iterations for LLoyd's Algorithm"""
@@ -86,6 +90,17 @@ class Patch:
     def iou(self, other: Patch) -> float:
         return self.intersection(other) / self.union(other)
 
+    def width(self):
+        return self.bottom_right[0] - self.top_left[0]
+
+    def height(self):
+        return self.bottom_right[1] - self.top_left[1]
+
+    def contains(self, point: Tuple[int, int]):
+        x, y = point
+
+        return self.top_left[0] <= x <= self.bottom_right[0] and self.top_left[1] <= y <= self.bottom_right[1]
+
 
 def nms(patches: List[Patch], threshold: float) -> List[Patch]:
     patches.sort(key=lambda p: p.score, reverse=True)
@@ -122,64 +137,60 @@ def gen_heatmap(ifs: Dict[int, Coords], image_width: int, image_height: int, gri
     return hmap
 
 
-# query_word is a numpy matrix of the image data
-def wordspotting(page: str, query_patch, settings: WordspottingSettings):
-    document_image_filename = os.path.join(os.path.dirname(__file__), 'pages/%s.png' % page)
-    image = Image.open(document_image_filename)
-    # Fuer spaeter folgende Verarbeitungsschritte muss das Bild mit float32-Werten vorliegen.
-    im_arr = np.asarray(image, dtype='float32')
+def patch_index(a, patch):
+    """Indexes the given np array with the given patch"""
+    return a[patch.top_left[1]:patch.bottom_right[1], patch.top_left[0]:patch.bottom_right[0]]
 
-    pickle_densesift_fn = 'SIFT/%s-full_dense-%d_sift-%d_descriptors.p' \
-                          % (page, settings.sift_step_size, settings.sift_cell_size)
-    frames, descriptors = pickle.load(open(pickle_densesift_fn, 'rb'))
 
-    visual_words, labels = kmeans2(descriptors, settings.dictionary_size, iter=settings.lloyd_iterations,
-                                   minit='points')
-
+def preprocess(frames, descriptors, settings):
+    visual_vocabulary, labels = kmeans2(descriptors, settings.dictionary_size, iter=settings.lloyd_iterations,
+                                        minit='points')
     ifs = inverse_file_structure(frames, labels)
 
-    query_frames = []
+    return ifs, visual_vocabulary
+
+
+# query_word is a numpy matrix of the image data
+def wordspotting(image_array, frames: Coords, descriptors, ifs: Dict[int, Coords], vocab, query_patch: Patch,
+                 settings: WordspottingSettings):
     query_desc = []
-    # query_frames, query_desc = compute_sift_descriptors(query_word, settings.sift_cell_size, settings.sift_step_size)
     # NOTE: We walk over all descriptors here. We could instead calculate which descriptors lie in the query_patch
-    for ((x, y), descriptor) in zip(frames, descriptors):
-        if query_patch.top_left[0] <= x <= query_patch.bottom_right[0] and query_patch.top_left[1] <= y <= \
-                query_patch.bottom_right[1]:
-            query_frames.append((x, y))
+    for (p, descriptor) in zip(frames, descriptors):
+        if query_patch.contains(p):
             query_desc.append(descriptor)
 
     # noinspection PyUnresolvedReferences
-    query_words = np.argmin(cdist(visual_words, np.array(query_desc)), axis=0)
+    query_words = np.argmin(cdist(vocab, np.array(query_desc)), axis=0)
 
-    heatmap = gen_heatmap(ifs, im_arr.shape[1], im_arr.shape[0], settings.heatmap_grid_width,
+    height, width = image_array.shape
+    heatmap = gen_heatmap(ifs, width, height, settings.heatmap_grid_width,
                           settings.heatmap_grid_height, query_words)
-    heatmap = cv2.resize(heatmap, dsize=(image.width, image.height),
+    heatmap = cv2.resize(heatmap, dsize=(width, height),
                          interpolation=settings.heatmap_interpolation_method)
 
-    patch_width = query_patch.bottom_right[0] - query_patch.top_left[0]
-    patch_height = query_patch.bottom_right[1] - query_patch.top_left[1]
+    patch_width = query_patch.width()
+    patch_height = query_patch.height()
     patches = []
 
-    start_x = int(patch_width / 2.0)
-    start_y = int(patch_height / 2.0)
+    start_x = int(query_patch.width() / 2.0)
+    start_y = int(query_patch.height() / 2.0)
 
-    step_x = int(patch_width / 8.0)
-    step_y = int(patch_height / 8.0)
+    step_x = int(patch_width * settings.patch_step[0] if settings.patch_step[0] < 1.0 else settings.patch_step[0])
+    step_y = int(patch_height * settings.patch_step[1] if settings.patch_step[1] < 1.0 else settings.patch_step[1])
 
-    for x in range(start_x, im_arr.shape[1], step_x):
-        for y in range(start_y, im_arr.shape[0], step_y):
+    for x in range(start_x, width, step_x):
+        for y in range(start_y, height, step_y):
             patch = Patch(x, y, patch_width, patch_height, 0.0)
-            hmax = heatmap[int(patch.top_left[1]):int(patch.bottom_right[1]),
-                   int(patch.top_left[0]):int(patch.bottom_right[0])].max()
-            patch.score = heatmap[int(patch.top_left[1]):int(patch.bottom_right[1]),
-                          int(patch.top_left[0]):int(patch.bottom_right[0])].sum()
+            patch_array = patch_index(heatmap, patch)
+            hmax = patch_array.max()
+            patch.score = patch_array.sum()
             if hmax > settings.patch_threshold:
                 patches.append(patch)
 
     nms_patches = nms(patches, settings.nms_threshold)
 
     if settings.show_plots:
-        plt.imshow(im_arr[query_patch.top_left[1]:query_patch.bottom_right[1], query_patch.top_left[0]:query_patch.bottom_right[0]], cmap="Greys_r")
+        plt.imshow(patch_index(image_array, query_patch), cmap="Greys_r")
         plt.show()
         fig = plt.figure()
         heatmap_plot = fig.add_subplot(1, 3, 1)
@@ -187,23 +198,21 @@ def wordspotting(page: str, query_patch, settings: WordspottingSettings):
         patch_plot = fig.add_subplot(1, 3, 3)
 
         heatmap_plot.set_title("Heatmap (Interpolated)")
-        heatmap_plot.imshow(image, cmap="Greys_r")
+        heatmap_plot.imshow(image_array, cmap="Greys_r")
         heatmap_plot.imshow(heatmap, cmap="Reds", alpha=0.4)
 
         heatmap_threshold = np.where(heatmap >= settings.patch_threshold, heatmap, 0.0)
         threshold_plot.set_title("Heatmap (Interpolated, Threshold: %g)" % settings.patch_threshold)
-        threshold_plot.imshow(image, cmap="Greys_r")
+        threshold_plot.imshow(image_array, cmap="Greys_r")
         threshold_plot.imshow(heatmap_threshold, cmap="Reds", alpha=0.4)
 
         patch_plot.set_title("NMS Patches")
-        patch_plot.imshow(image, cmap="Greys_r")
+        patch_plot.imshow(image_array, cmap="Greys_r")
         patch_plot.imshow(heatmap, cmap="Reds", alpha=0.4)
 
         for patch in nms_patches:
             lower_left = (patch.top_left[0], patch.bottom_right[1])
-            width = (patch.bottom_right[0] - patch.top_left[0])
-            height = (patch.bottom_right[1] - patch.top_left[1])
-            rect = Rectangle(lower_left, width, -height, alpha=0.5)
+            rect = Rectangle(lower_left, patch.width(), -patch.height(), alpha=0.5)
             patch_plot.add_patch(rect)
 
         plt.show()
@@ -261,17 +270,54 @@ def read_groundtruth(page: str) -> Dict[str, List[Patch]]:
     return result
 
 
-def main():
-    page = "2700270"
+def load_page(page: str, settings: WordspottingSettings):
     gt = read_groundtruth(page)
 
-    patch = gt["of"][0]
-    patches = wordspotting(page, patch, WordspottingSettings())
+    document_image_filename = os.path.join(os.path.dirname(__file__), 'pages/%s.png' % page)
+    image = Image.open(document_image_filename)
+    # Fuer spaeter folgende Verarbeitungsschritte muss das Bild mit float32-Werten vorliegen.
+    im_arr = np.asarray(image, dtype='float32')
 
-    precision, recall, average_precision = analyze(patches, gt["the"])
+    assert (image.size[0] == im_arr.shape[1])
+    assert (image.size[1] == im_arr.shape[0])
+
+    pickle_densesift_fn = 'SIFT/%s-full_dense-%d_sift-%d_descriptors.p' \
+                          % (page, settings.sift_step_size, settings.sift_cell_size)
+    frames, descriptors = pickle.load(open(pickle_densesift_fn, 'rb'))
+
+    return im_arr, frames, descriptors, gt
+
+
+def analyze_page(page: str, settings: WordspottingSettings):
+    precision, recall, average_precision = 0.0, 0.0, 0.0
+    runs = 0
+    im_arr, frames, descriptors, gt = load_page(page, settings)
+    ifs, vocab = preprocess(frames, descriptors, settings)
+
+    for word in gt:
+        for i, patch in enumerate(gt[word]):
+            patches = wordspotting(im_arr, frames, descriptors, ifs, vocab, patch, settings)
+            run_precision, run_recall, run_average_precision = analyze(patches, gt[word])
+            print(word, " (", i + 1, " / ", len(gt[word]), "): Precision: ", run_precision, ", Recall: ", run_recall,
+                  ", Average Precision: ", run_average_precision)
+            runs += 1
+            precision += run_precision
+            recall += run_recall
+            average_precision += run_average_precision
+
+    return precision / runs, recall / runs, average_precision / runs
+
+
+def main():
+    page = "2700270"
+    settings = WordspottingSettings()
+    settings.show_plots = False
+    precision, recall, average_precision = analyze_page(page, settings)
+
     print("Precision: ", precision)
     print("Recall: ", recall)
     print("Average Precision: ", average_precision)
+    print("Values averaged over all possible input words")
 
 
 if __name__ == '__main__':
